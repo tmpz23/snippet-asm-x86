f64c8e22e0c03ab3c550209e7949181f55b6100d
tmpz23 <tmpz23@laposte.net>
2017-05-17 17:47:23 +0200

    new commit3

--- HexDump32.asm
+++ HexDump32.asm
@@ -6,43 +6,50 @@ includelib	C:\masm32\lib\msvcrt.lib
 ; use : invoke HexDump32, lpData, 32
 ; use : invoke HexDump32, lpData, 123
 
-HexDump32 PROC USES ecx esi ebx edx lpData:DWORD, dwLen:DWORD
-	invoke crt_printf, ADDR str_intro ; Affiche le titre du dump
-	
-	mov esi, lpData
-	invoke crt_printf, ADDR format_dwAddr, esi
-	
-	mov ecx, dwLen
-	xor edx,edx
-	
-	HD_loop :
-		push ecx
-		push edx
+.data
+	str_intro 		db "Dump :",0
+	str_crlf		db 13,10,0
+	format_2x 		db " %.02x",0
+	format_dwAddr 	db 10,13,"%.08x :",0
 
-		movzx ebx, BYTE PTR [esi]
-		invoke crt_printf, ADDR format_2x, ebx
-
-		pop edx
-		pop ecx
-
-		inc edx
-		inc esi
+.code
+	HexDump32 PROC USES ecx esi ebx edx lpData:DWORD, dwLen:DWORD
+		invoke crt_printf, ADDR str_intro ; Affiche le titre du dump
 		
-		cmp edx,16
-
-		jb HD_continuerLigne
-		cmp ecx,1
-		jz HD_continuerLigne
+		mov esi, lpData
+		invoke crt_printf, ADDR format_dwAddr, esi
 		
-		push ecx
-		invoke crt_printf, ADDR format_dwAddr, esi ; On affiche l'adresse de dump actuelle
-		pop ecx
+		mov ecx, dwLen
 		xor edx,edx
+		
+		HD_loop :
+			push ecx
+			push edx
 
-		HD_continuerLigne:
-		loop HD_loop
+			movzx ebx, BYTE PTR [esi]
+			invoke crt_printf, ADDR format_2x, ebx
 
-	invoke crt_printf, ADDR str_crlf ; Saut de ligne final
+			pop edx
+			pop ecx
 
-	ret
-HexDump32 ENDP
+			inc edx
+			inc esi
+			
+			cmp edx,16
+
+			jb HD_continuerLigne
+			cmp ecx,1
+			jz HD_continuerLigne
+			
+			push ecx
+			invoke crt_printf, ADDR format_dwAddr, esi ; On affiche l'adresse de dump actuelle
+			pop ecx
+			xor edx,edx
+
+			HD_continuerLigne:
+			loop HD_loop
+
+		invoke crt_printf, ADDR str_crlf ; Saut de ligne final
+
+		ret
+	HexDump32 ENDP
